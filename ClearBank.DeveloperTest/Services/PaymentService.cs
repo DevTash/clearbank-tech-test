cd16ca64bfc4468ff40ea3e8fa3ea7443d88a73f
Tashan Lewis <tashan.lewis@outlook.com>
2025-10-24 14:01:31 +0100

    🖥️ wip: Inject validators into payment service

--- ClearBank.DeveloperTest/Services/PaymentService.cs
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -1,73 +1,34 @@
 ﻿using ClearBank.DeveloperTest.Data;
 using ClearBank.DeveloperTest.Types;
+using ClearBank.DeveloperTest.Validators;
 
 namespace ClearBank.DeveloperTest.Services
 {
     public class PaymentService : IPaymentService
     {
         private readonly IDataStoreFactory _dataStoreFactory;
+        private readonly IPaymentRequestValidatorFactory _paymentRequestValidatorFactory;
 
-        public PaymentService(IDataStoreFactory dataStoreFactory)
+        public PaymentService(
+            IDataStoreFactory dataStoreFactory,
+            IPaymentRequestValidatorFactory paymentRequestValidatorFactory
+        )
         {
             _dataStoreFactory = dataStoreFactory;
+            _paymentRequestValidatorFactory = paymentRequestValidatorFactory;
         }
 
         public MakePaymentResult MakePayment(MakePaymentRequest request)
         {
             var account = _dataStoreFactory.Get().GetAccount(request.DebtorAccountNumber);
+            var validator = _paymentRequestValidatorFactory.Get(request.PaymentScheme);
 
             var result = new MakePaymentResult();
-
-            result.Success = true;
+            result.Success = validator?.IsValid(account, request) ?? false;
             
-            switch (request.PaymentScheme)
-            {
-                case PaymentScheme.Bacs:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
-                    {
-                        result.Success = false;
-                    }
-                    break;
-
-                case PaymentScheme.FasterPayments:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Balance < request.Amount)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-
-                case PaymentScheme.Chaps:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Status != AccountStatus.Live)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-            }
-
             if (result.Success)
             {
                 account.Balance -= request.Amount;
-
                 _dataStoreFactory.Get().UpdateAccount(account);
             }
 
